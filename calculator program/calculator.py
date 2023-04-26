25b83e833a3caab830b17b250ff670a0262d3b7a
V.Bhanusri <120394926+BHANUSRIA3@users.noreply.github.com>
2023-04-26 15:55:03 +0530

    Add files via upload

---
+++ calculator program/calculator.py
@@ -0,0 +1,8 @@
+#3
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+print("sum is ",a+b)
+print("difference is ",a-b)
+print("product is",a*b)
+print("division is ",a/b)
+print("mod is ",a%b)
